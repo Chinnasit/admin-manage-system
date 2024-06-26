cf5d2ac9869e2be31c5043b4b45b718d8dcbcf5e
Chinnasit <76206065+Chinnasit@users.noreply.github.com>
2024-06-26 23:51:23 +0700

    add struct UserResponse

--- backend/entities/user.go
+++ backend/entities/user.go
@@ -1,14 +1,27 @@
 package entities
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type User struct {
 	gorm.Model
-	ID        uint   `gorm:"primaryKey"`
+	ID        uint    `gorm:"primaryKey"`
 	FirstName *string `gorm:"not null" `
 	LastName  *string `gorm:"not null" `
 	Email     *string `gorm:"unique;not null`
 	Password  *string `gorm:"not null" `
 	RoleId    *uint   `gorm:"not null" `
-	Active    bool   `gorm:"not null;default:false"`
+	Active    bool    `gorm:"not null;default:false"`
+}
+
+type UserResponse struct {
+	ID        uint      `json:"id"`
+	Email     string    `json:"email"`
+	FullName  string    `json:"fullName"`
+	RoleId    uint      `json:"roleId"`
+	CreatedAt time.Time `json:"createdAt"`
+	Active    bool      `json:"active"`
 }
